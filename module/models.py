8d2419070e4fbc764f5475a4f4b1ce5dc1d5e5dd
nina <nb114@hdm-stuttgart.de>
2022-12-29 19:07:33 +0100

    Content wird automatisch aus Backend gepullt

--- module/models.py
+++ module/models.py
@@ -10,25 +10,26 @@ class Modul(models.Model):
         return str(self.nummer) + ' | ' + self.name #In Admin-Oberfläche wird Name des Moduls angezeigt
         
 class Unterseite(models.Model):
-    titel = models.CharField(max_length=30)
-    modulname = models.ForeignKey(Modul, on_delete=models.CASCADE)
+    titel = models.CharField(max_length=100)
+    modul = models.ForeignKey(Modul, on_delete=models.CASCADE)
     nummer = models.IntegerField(default=0)
+    naechste = models.IntegerField(default=-1)
     inhalt = models.TextField(max_length=2000)
     class Meta:
         verbose_name_plural = "Unterseiten"
     def __str__(self):
-        return str(self.modulname) + ' | ' + str(self.nummer) + ' | ' + self.titel #In Admin-Oberfläche wird Name des Moduls angezeigt
+        return str(self.modul) + ' | ' + str(self.nummer) + ' | ' + self.titel #In Admin-Oberfläche wird Name des Moduls angezeigt
         
 class Frage(models.Model):
     frage_id = models.IntegerField(default=0)
     frage_titel = models.CharField(max_length=200, default='')
     frage_satz = models.TextField(max_length=200)
-    frage_modul = models.ForeignKey(Modul, on_delete=models.CASCADE)
+    modul = models.ForeignKey(Modul, on_delete=models.CASCADE)
     erklaerung = models.TextField(max_length=1000, blank=True, default='')
     class Meta:
             verbose_name_plural = "fragen"
     def __str__(self):
-        return str(self.frage_modul) + ' | ' + self.frage_titel #In Admin-Oberfläche wird Fragetitel und ID der Frage gezeigt
+        return str(self.modul) + ' | ' + self.frage_titel #In Admin-Oberfläche wird Fragetitel und ID der Frage gezeigt
 
 class Antwort(models.Model):
     antwort_id = models.IntegerField(default=0)
