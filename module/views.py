467057b1b19ad5d72a41459c9a976e44206ee557
nina <nb114@hdm-stuttgart.de>
2022-12-30 18:43:01 +0100

    Templates von User in Module verschoben

--- module/views.py
+++ module/views.py
@@ -6,10 +6,10 @@ from . import models
 # Create your views here.
 
 def index(request):
-    return render(request, 'users/index.html')
+    return render(request, 'module/index.html')
 
 def einfuehrung(request, modul_id, unterseite_id):
     modul = models.Modul.objects.get(nummer=modul_id)
     unterseite = models.Unterseite.objects.get(nummer=unterseite_id, modul = modul)
     
-    return render(request, 'users/einfuehrung.html', {'modul': modul, 'unterseite' : unterseite})
+    return render(request, 'module/einfuehrung.html', {'modul': modul, 'unterseite' : unterseite})
